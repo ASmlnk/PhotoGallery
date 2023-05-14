41d32cbf54234586ea9f1a6a2c63267d1ab2a1f6
DelaNx <sdelanss@gmail.com>
2023-05-14 18:42:50 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ app/src/main/java/combignerdranch/android/photogallery/GalleryItem.kt
@@ -0,0 +1,7 @@
+package combignerdranch.android.photogallery
+
+data class GalleryItem(
+    var title: String = "",
+    var id: String = "",
+    var url: String = ""
+)
--- app/src/main/java/combignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/combignerdranch/android/photogallery/api/FlickrApi.kt
@@ -5,7 +5,13 @@ import retrofit2.http.GET
 
 interface FlickrApi {
 
-    @GET("/")
-    fun fetchContents(): Call<String>
+    @GET(
+        "service/rest/?method=flickr.interestingness.getList" +
+                "&api_key=e6dbf158a5322e590962f3653190009a" +   //код ролученый на сайте
+                "&format=json" +                                //по умолчанию формат xml, мы указываем json
+                "&nojsoncallback=1" +                           //указываем убрать все круглые скобки в ответе
+                "&extras=url_s"                                 //добавить url_s адрес мини-версии изображения если он есть
+    )
+    fun fetchPhotos(): Call<String>
 
 }
