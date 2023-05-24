bd11ec6e5b5defc5d28a3255d3a266cc419c2fc7
user <sdelanss@gmail.com>
2023-05-24 05:47:58 +0300

    git pull origin master --allow-unrelated-histories

--- app/src/main/java/combignerdranch/android/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/combignerdranch/android/photogallery/PhotoGalleryViewModel.kt
@@ -1,12 +1,10 @@
 package combignerdranch.android.photogallery
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
-import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import java.security.Provider
 
 class PhotoGalleryViewModel (private val photoGalleryPageRepository: PhotoGalleryPageRepository): ViewModel() {
@@ -22,7 +20,7 @@ class PhotoGalleryViewModel (private val photoGalleryPageRepository: PhotoGaller
     }
 
     fun getMovieList(): LiveData<PagingData<GalleryItem>> {
-        return photoGalleryPageRepository.getAllGalleryItems()
+        return photoGalleryPageRepository.getAllGalleryItems().cachedIn(viewModelScope)
     }
 
 
