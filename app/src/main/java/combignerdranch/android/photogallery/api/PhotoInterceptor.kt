6c59365eb268a09f7c86781b1a81baeabb4dcc3c
user <sdelanss@gmail.com>
2023-06-19 14:09:50 +0300

    git pull origin master --allow-unrelated-histories

--- app/src/main/java/combignerdranch/android/photogallery/api/PhotoInterceptor.kt
+++ app/src/main/java/combignerdranch/android/photogallery/api/PhotoInterceptor.kt
@@ -1,18 +1,16 @@
 package combignerdranch.android.photogallery.api
 
-import android.util.Log
 import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 
 private const val API_KEY = "e6dbf158a5322e590962f3653190009a" //api ключ полученый на сайте
-private const val TAG = "MY"
 
 class PhotoInterceptor: Interceptor {   //перехвадчик
     override fun intercept(chain: Interceptor.Chain): Response {
+
         val originalRequest: Request = chain.request() //функция для доступа к исходному запросу
-        Log.i(TAG,"$API_KEY")
         val newUrl: HttpUrl = originalRequest.url.newBuilder()
             .addQueryParameter("api_key", API_KEY)
            .addQueryParameter("format", "json")
@@ -20,7 +18,6 @@ class PhotoInterceptor: Interceptor {   //перехвадчик
             .addQueryParameter("extras", "url_s")
             .addQueryParameter("safesearch", "1")
             .build()
-        Log.i(TAG,"$API_KEY")
 
         val newRequest: Request = originalRequest.newBuilder()
             .url(newUrl)
